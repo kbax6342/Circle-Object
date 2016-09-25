e0623500bec42b1fbc214cbdfea848a5cd16f0dc
Kevin Baxter <kbax6342@gmail.com>
2016-09-25 18:08:06 -0400

    Initial Commit

---
+++ src/CircleApp.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+
+public class CircleApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Declarations
+		double radius;
+		
+		//Utilities
+		Scanner input = new Scanner (System.in);
+		
+		
+
+		//Welcome to the Application
+		System.out.println("Welcome to the Circl App\n");
+		
+		//Caption describing the program
+		System.out.println("~ ~This is where you can input any radius and you can creat a circle!!!~ ~");
+		
+		//Prompt the user to enter a radius
+		System.out.println("Please enter the radius: ");
+		radius = input.nextInt();
+		
+		
+		
+		
+		
+		//The application displays an error if the user enters invalid data
+		//When the user enters valid that the application calculates  the area and
+		//circumference of the circle and rounds to the nearest 2 decimal places.
+		//The application displays a good bye message that also indicates the number of circles the user bild when t e user chooes not to continue
+		
+		
+	}
+
+}
